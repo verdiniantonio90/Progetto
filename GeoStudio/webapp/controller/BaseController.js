ed58a03fa1113d6f61b38ca1a3f8a2403ddaef9c
verdiniantonio90 <verdiniantonio90@gmail.com>
2025-10-23 16:34:01 +0200

    aggiunta funzione front-end createAnag

--- GeoStudio/webapp/controller/BaseController.js
+++ GeoStudio/webapp/controller/BaseController.js
@@ -3,95 +3,134 @@ sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/ui/core/UIComponent",
   "sap/m/MessageBox"
-], function (Controller, UIComponent,MessageBox) {
+], function (Controller, UIComponent, MessageBox) {
 
   "use strict";
- 
+
   return Controller.extend("my.application.controller.BaseController", {
- 
-    openDialog : function(nomeDialog){
-        debugger;
-        var path = "GeoStudio.view."+nomeDialog
-         if (!this[nomeDialog]) {
-                this[nomeDialog] = sap.ui.xmlfragment(path, this);
-                this.getView().addDependent(this[nomeDialog]);
-            }
-            this[nomeDialog].open();
+
+    openDialog: function (nomeDialog) {
+      debugger;
+      var path = "GeoStudio.view." + nomeDialog
+      if (!this[nomeDialog]) {
+        this[nomeDialog] = sap.ui.xmlfragment(path, this);
+        this.getView().addDependent(this[nomeDialog]);
+      }
+      this[nomeDialog].open();
     },
-    closeDialog(nomeDialog, id){
-        
-        this[nomeDialog] = null;
-        sap.ui.getCore().byId(id).destroy();
+    closeDialog(nomeDialog, id) {
+
+      this[nomeDialog] = null;
+      sap.ui.getCore().byId(id).destroy();
     },
-    read : function(url, headers, nameModel){
-           $.ajax({
-                method: 'GET',
-                url: url,
-                headers: headers,
-                contentType: 'application/json',
-                dataType: 'json',
-                success: function (oResponse) {
-                    oResponse.data ? this.getView().getModel(nameModel).setData(oResponse.data) : this.getView().getModel(nameModel).setData([]);
-                }.bind(this),
-                error: function (oError) {
+    read: function (url, headers, nameModel) {
+      $.ajax({
+        method: 'GET',
+        url: url,
+        headers: headers,
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function (oResponse) {
+          oResponse.data ? this.getView().getModel(nameModel).setData(oResponse.data) : this.getView().getModel(nameModel).setData([]);
+        }.bind(this),
+        error: function (oError) {
 
 
-                }.bind(this)
-            });
+        }.bind(this)
+      });
     },
-    createAnagraficaDoc : function(oEntry, oEntryDoc){
+    createAnagraficaDoc: function (oEntry, oEntryDoc) {
       var Anagrafica = this.checkInserAnagrafica(oEntry);
       // se anagrafica true allora checkDocumenti
-      if(Anagrafica){
+      if (Anagrafica) {
         var documenti = this.checkDocumenti(oEntryDoc);
-        if(documenti){
-          
+        if (documenti) {
+
         }
       }
       // se CheckDocumenti true inserisci tutto in tabella
     },
-    checkInserAnagrafica : function(oEntry){
-        var message = ""
-        message += oEntry.telefono && oEntry.cellulare ? ""  : "telefono o cellulare \n";
-        message += oEntry.cf && oEntry.p_iva ? "" : "Codice Fiscale o Partita Iva \n";
-        message += oEntry.nome ? "" : "Nome \n";
-        message += oEntry.cognome ? "" : "Cognome \n";
-        
-        var sMs = message.length > 0 ? "Inserire i seguenti campi: \n" + message : "";
-        if(sMs){
-          MessageBox.error(sMs)
-          return false
-        }else{
-          return true
-        }
+    checkInserAnagrafica: function (oEntry) {
+      var message = ""
+      message += oEntry.telefono && oEntry.cellulare ? "" : "telefono o cellulare \n";
+      message += oEntry.cf && oEntry.p_iva ? "" : "Codice Fiscale o Partita Iva \n";
+      message += oEntry.nome ? "" : "Nome \n";
+      message += oEntry.cognome ? "" : "Cognome \n";
+
+      var sMs = message.length > 0 ? "Inserire i seguenti campi: \n" + message : "";
+      if (sMs) {
+        MessageBox.error(sMs)
+        return false
+      } else {
+        return true
+      }
 
     },
-    checkDocumenti : function(oEntry){
-     
+    checkDocumenti: function (oEntry) {
+
       var message = ""
       var aCheck = []
-      if(oEntry){
+      if (oEntry) {
         var oData = oEntry
-        for( var i = 0 ; i < oData.length; i++){   
+        for (var i = 0; i < oData.length; i++) {
           oData[i].tipologia ? aCheck.push(true) : aCheck.push("Tipologia");
           oData[i].num_doc ? aCheck.push(true) : aCheck.push("Numero Documento");
         }
         message += aCheck.includes("Tipologia") ? "Tipologia \n" : "";
         message += aCheck.includes("Numero Documento") ? "Numero Documento \n" : "";
         var sMs = message.length > 0 ? "Inserire i seguenti campi: \n" + message : "";
-        if(sMs){
+        if (sMs) {
           MessageBox.error(sMs)
           return false;
-        }else{
+        } else {
           return true;
         }
       }
 
+    },
+    createAnagDoc: function (anagrafica, documenti) {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      const raw = JSON.stringify({
+        "anagrafica": {
+          "id": "",
+          "nome": "Luigi",
+          "cognome": "fava",
+          "indirizzo": "via trevi 34",
+          "cap": "00100",
+          "dataNascita": "04/10/1990",
+          "telefono": "06308308",
+          "cellulare": "3588789032",
+          "email": "prova1@mail.com",
+          "pec": "pec1@mail.com",
+          "cf": "afgijuiffegh",
+          "p_iva": "01334947857"
+        },
+        "documenti": [
+          {
+            "fk_id": "",
+            "tipologia": "PA",
+            "num_doc": "AY789TR"
+          }
+        ]
+      });
+
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+      };
+
+      fetch("http://localhost:4000/CreateAnagDoc", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
     }
 
 
 
- 
   });
- 
+
 });
